94333c016239561d97ec800d6c0c08037c96a9cc
Fanny-Yuan <1159710144@qq.com>
2022-06-10 16:49:56 +0100

    my commit

---
+++ views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <TopNav />
+    <SwiperTop />
+    <IconList/>
+    <MusicList/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import { getMusicItemList, getItemList } from "@/request/api/item.js";
+import TopNav from "@/components/home/TopNav.vue";
+import SwiperTop from "@/components/home/SwiperTop.vue";
+import IconList from "@/components/home/IconList.vue";
+import MusicList from "@/components/home/MusicList.vue";
+export default {
+  name: "Home",
+  // async mounted(){
+  //     let res2= await getItemList(5221080943);
+  //     console.log(res2);
+  // },
+  components: {
+    TopNav,
+    SwiperTop,
+    IconList,
+    MusicList,
+  },
+};
+</script>
